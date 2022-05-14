bba4e78e871e66a5972bdb02746ee7fb4e3d0db0
Ali <arap_ali_35@hotmail.com>
2022-05-14 21:02:55 +0300

    Ders6

---
+++ Ders6/KendiFormum/Ders-6.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ders 6</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="Ders-6.css">
+</head>
+<body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ 
+
+<div class="container" style="color:silver">
+        <h1 class="deneme">Maviden Yeşile Kusursuz Bir Cevap
+        <p></p>
+    
+<form>
+  <div class="form-md">
+    <div class="form-group col-md-3">
+      <label for="inputEmail4" style="color:white">İsim</label>
+      <input type="İsim Giriniz " class="form-control" id="inputname4" placeholder="İsim">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="inputPassword4"style="color:white">Soyad</label>
+      <input type="Soy İsim Giriniz" class="form-control" id="inputPassword4" placeholder="Soyad">
+    </div>
+  </div>
+  <div class="form-group col-md-3">
+    <label for="inputAddress"style="color:white">E Posta</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="E Posta Giriniz ">
+  </div>
+  <div class="form-group col-md-3">
+    <label for="inputAddress2"style="color:white">Numara</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Numara Giriniz">
+  </div>
+ 
+  <div class="form-group col-md-3">
+      <label for="inputState"style="color:white">Ders Seçiniz</label>
+      <select id="inputState" class="form-control">
+        <option selected>Web Editörü</option>
+    <option value="">Web Programlama</option>
+    <option value="">Sunucu İşletim Sistemleri</option>
+      </select>
+    </div>
+  
+  
+  <div class="form-md">
+    <div class="form-group col-md-3">
+      <label for="inputCity"style="color:white">Notunuz</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+   
+ 
+    <div class="d-grid gap-5 col-10 mx-auto">
+    <button type="button" class="btn btn-danger">Gönder</button>
+</div>
+</form>
+</div>
+
+
+<?php
+
+
+?>
+
+
+
+</body>
+</html>
